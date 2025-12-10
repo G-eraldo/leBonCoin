cc0af33c55c1e1e081db703290b94018b75bdea0
G_eraldo <gerald-f@hotmail.fr>
2025-12-10 16:02:24 +0100

    populate double

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -5,9 +5,15 @@ module.exports = (plugin) => {
     }
 
     const user = await strapi.entityService.findOne(
-      'plugin::users-permissions.user',
+      "plugin::users-permissions.user",
       ctx.state.user.id,
-      { populate: ['userProfile'] }
+      {
+        populate: {
+          userProfile: {
+            populate: ["adress"],
+          },
+        },
+      }
     );
 
     ctx.body = user;
